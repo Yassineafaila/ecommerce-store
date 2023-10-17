992d1439762e2942647dfc710fc4834cf0be6cb0
Yassineafaila <yassineafaila55@gmail.com>
2023-10-17 13:32:55 +0100

    style:add the sytles of the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import "./Footer.scss"
 function Footer() {
   return (
-    <footer>Footer</footer>
-  )
+    <footer>
+      <div>
+        <p>by Me :</p>
+        <Link to="https://github.com/Yassineafaila" target="_blank">
+          Yassine Afaila
+        </Link>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
