fcfc2a3c103b7f059c05bb4e6bdd8576a206c5e9
Yassineafaila <yassineafaila55@gmail.com>
2023-10-20 17:25:51 +0100

    refactor: change the code from using the useState to useReducer

--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,23 +1,54 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
 
 const ProductsContext = createContext();
+const initialState = {
+  products: [],
+  CartProducts: [],
+  likedProducts: [],
+  TotalPrice: 0,
+  isLoading: false,
+  currentProduct: [],
+  error:""
+}
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {...state,isLoading:true}
+    case "products/loaded":
+      return { ...state,isLoading:false, products: action.payload }
+    case "product/loaded":
+      return { ...state, isLoading: false, currentProduct: action.payload }
+    case "addCart":
+      return { ...state, CartProducts: action.payload }
+    case "CalculTotalPrice":
+      return { ...state, TotalPrice: action.payload }
+    case "addLikedProduct":
+      return {...state,likedProducts:action.payload}
+    case "rejected":
+      return {...state,isLoading:false,error:action.payload}
+
+  }
+}
 function ProductsProvider({ children }) {
-  const [products, setProducts] = useState([]);
-  const [CartProducts, setCartProducts] = useState([]);
-  const [likedProducts, setLikedProducts] = useState([]);
-  const [TotalPrice, setTotalPrice] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentProduct, setCurrentProduct] = useState([]);
+  // const [products, setProducts] = useState([]);
+  // const [CartProducts, setCartProducts] = useState([]);
+  // const [likedProducts, setLikedProducts] = useState([]);
+  // const [TotalPrice, setTotalPrice] = useState(0);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [currentProduct, setCurrentProduct] = useState([]);
+  let [{products,CartProducts,likedProducts,TotalPrice,isLoading,currentProduct},dispatch]=useReducer(reducer,initialState)
   useEffect(() => {
     const fetchProducts = async () => {
+      dispatch({type:"loading"})
       try {
         const response = await fetch("https://fakestoreapi.com/products");
         const data = await response.json();
-        setProducts(data);
+        dispatch({type:"products/loaded",payload:data})
       } catch (error) {
-        console.log("Error while fetching the products", error);
-      } finally {
-        setIsLoading(false);
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading products ...",
+        });
       }
     };
     fetchProducts();
@@ -27,14 +58,17 @@ function ProductsProvider({ children }) {
   },[CartProducts])
   async function getProductInfo(id) {
     const fetchProduct = async () => {
+      dispatch({ type: "loading" });
       try {
         const response = await fetch(`https://fakestoreapi.com/products/${id}`);
         const data = await response.json();
-        setCurrentProduct(data);
+        dispatch({ type:"product/loaded",payload:data})
       } catch (error) {
         console.log("Error while fetching the product details", error);
-      } finally {
-        setIsLoading(false);
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading product ...",
+        });
       }
     };
     fetchProduct();
@@ -50,10 +84,18 @@ function ProductsProvider({ children }) {
       const product = products.find((item) => item.id === id);
 
       if (product) {
-        setCartProducts((prevCartProducts) => [
-          ...prevCartProducts,
-          { ...product, quantity: 1, subTotal: product.price },
-        ]);
+        // setCartProducts((prevCartProducts) => [
+        //   ...prevCartProducts,
+        //   { ...product, quantity: 1, subTotal: product.price },
+        // ]);
+        // CartProducts=[...CartProducts,{...product,quantity:1,subTotal:product.price}]
+        dispatch({
+          type: "addCart",
+          payload: [
+            ...CartProducts,
+            { ...product, quantity: 1, subTotal: product.price },
+          ],
+        });
       }
     }
 
@@ -65,7 +107,9 @@ function ProductsProvider({ children }) {
     if (productCart) {
       productCart.quantity += 1;
       productCart.subTotal = productCart.quantity * productCart.price;
-      setCartProducts([...CartProducts]);
+      // setCartProducts([...CartProducts]);
+      // CartProducts=[...CartProducts]
+      dispatch({ type: "addCart", payload: [...CartProducts] });
       calculateTotalPrice();
     }
     
@@ -81,20 +125,28 @@ function ProductsProvider({ children }) {
         productCart.quantity -= 1;
         productCart.subTotal = productCart.quantity * productCart.price;
       }
-      setCartProducts([...CartProducts]);
+      // setCartProducts([...CartProducts]);
+      // CartProducts = [...CartProducts];
+      dispatch({ type: "addCart", payload: [...CartProducts] });
       calculateTotalPrice();
     }
     
   };
   const RemoveProductFromTheCart = (id) => {
-    setCartProducts([...CartProducts.filter((product) => product.id !== id)]);
+    // setCartProducts([...CartProducts.filter((product) => product.id !== id)]);
+    // CartProducts = [...CartProducts.filter((product) => product.id !== id)];
+    dispatch({
+      type: "addCart",
+      payload: [...CartProducts.filter((product) => product.id !== id)],
+    });
   };
   const calculateTotalPrice = () => {
     const total = CartProducts.reduce(
       (acc, product) => acc + product.subTotal,
       0
     );
-    setTotalPrice(Math.round(total));
+    // setTotalPrice(Math.round(total));
+    dispatch({ type: "CalculTotalPrice",payload:total });
   };
   const AddLikedProducts = (id) => {
     const likedProduct = likedProducts.find((item) => item.id === id);
@@ -102,14 +154,22 @@ function ProductsProvider({ children }) {
       console.log("the product already liked");
     } else {
       const product = products.find((item) => item.id == id);
-      setLikedProducts([...likedProducts, {...product,liked:true}]);
+      // setLikedProducts([...likedProducts, {...product,liked:true}]);
+      dispatch({
+        type: "addLikedProduct",
+        payload: [...likedProducts, { ...product, liked: true }],
+      });
     }
   };
   const RemoveLikedProduct = (id) => {
     console.log("hi")
-    setLikedProducts((prevLikedProducts) =>
-      prevLikedProducts.filter((product) => product.id !== id)
-    );
+    // setLikedProducts((prevLikedProducts) =>
+    //   prevLikedProducts.filter((product) => product.id !== id)
+    // );
+    dispatch({
+      type: "addLikedProduct",
+      payload: likedProducts.filter((product) => product.id !== id),
+    });
   }
   return (
     <ProductsContext.Provider
