e0177b10fb5920696381a26ee3cafb71de90b0b3
Yassineafaila <yassineafaila55@gmail.com>
2023-10-21 10:30:52 +0100

    style:add the style to the About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import "./About.scss";
 function About() {
   return (
-    <div>About</div>
-  )
+    <main className="about_us">
+      <section className="about_us_container">
+        <article className='about_us_content'>
+          <h1>Our Story</h1>
+          <p>
+            Launced in 2015, Exclusive is South Asia’s premier online shopping
+            makterplace with an active presense in Bangladesh. Supported by wide
+            range of tailored marketing, data and service solutions, Exclusive
+            has 10,500 sallers and 300 brands and serves 3 millioons customers
+            across the region.
+          </p>
+          <p>
+            Exclusive has more than 1 Million products to offer, growing at a
+            very fast. Exclusive offers a diverse assotment in categories
+            ranging from consumer.
+          </p>
+        </article>
+        <div className='img__container'>
+          <img src={require("../../assets/icons/About-us.jpg")} alt='aboutImage'/>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default About
